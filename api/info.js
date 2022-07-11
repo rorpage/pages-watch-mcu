41b4eefc8174ff92f0d4d18206dd3cb5758a24ef
Robbie Page <robbie.page@e-gineering.com>
2022-07-11 12:47:41 -0400

    add keyv to API

--- api/info.js
+++ api/info.js
@@ -1,7 +1,14 @@
-export default function handler(req, res) {
+const Keyv = require('keyv');
+
+export default async function handler(req, res) {
+  const host = process.env.REDIS_URL || 'redis://localhost:6379';
+  const mcu = new Keyv(host, { namespace: 'mcu' });
+
+  mcu.on('error', err => console.log('Connection Error', err));
+
   let response = { startDate: 'June 10, 2022' };
 
-  response.index = 8;
+  response.index = await mcu.get('index');
 
   return res.json(response);
 }
