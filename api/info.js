46353f4e68246f3adb1f593825706c1b0a3fbda6
Robbie Page <robbie.page@e-gineering.com>
2022-07-12 13:10:35 -0400

    require keyv redis

--- api/info.js
+++ api/info.js
@@ -1,8 +1,11 @@
+const KeyvRedis = require('@keyv/redis');
 const Keyv = require('keyv');
 
 export default async function handler(req, res) {
   const host = process.env.REDIS_URL || 'redis://localhost:6379';
-  const mcu = new Keyv(host, { namespace: 'mcu' });
+
+  const keyvRedis = new KeyvRedis(host);
+  const mcu = new Keyv({ namespace: 'mcu', store: keyvRedis });
 
   mcu.on('error', err => console.log('Connection Error', err));
 
