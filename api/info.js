046fed9e7baa16db29aaa539fb76e47de9701e5e
Robbie Page <robbie.page@e-gineering.com>
2023-10-17 10:22:01 -0400

    add more series

--- api/info.js
+++ api/info.js
@@ -12,14 +12,23 @@ export default async function handler(req, res) {
   const data = await mcu.get('data');
 
   let background_color = '#000000';
+  let title = 'Marvel';
 
   if (data.series === 0) {
     background_color = '#e62429';
+    title = 'Marvel';
+  } else if (data.series === 1) {
+    background_color = '#000000';
+    title = 'Mission: Impossible';
+  } else if (data.series === 2) {
+    background_color = '#000000';
+    title = 'Star Wars';
   }
 
   const response = {
     ...data,
-    background_color
+    background_color,
+    title
   };
 
   return res.json(response);
---
+++ js/mission_impossible.js
@@ -0,0 +1,60 @@
+const missionImpossibleMovies = [
+  {
+    rating: 'PG-13',
+    release_date: 'May 22, 1996',
+    runtime: '1h 50m',
+    src: '/mi/mi1.jpg',
+    title: 'Mission: Impossible'
+  },
+  {
+    rating: 'PG-13',
+    release_date: 'May 24, 2000',
+    runtime: '2h 3m',
+    src: '/mi/mi2.jpg',
+    title: 'Mission: Impossible 2'
+  },
+  {
+    rating: 'PG-13',
+    release_date: 'May 5, 2006',
+    runtime: '2h 6m',
+    src: '/mi/mi3.jpg',
+    title: 'Mission: Impossible III'
+  },
+  {
+    rating: 'PG-13',
+    release_date: 'December 15, 2011',
+    runtime: '2h 13m',
+    src: '/mi/mi4.jpg',
+    title: 'Mission: Impossible - Ghost Protocol'
+  },
+  {
+    rating: 'PG-13',
+    release_date: 'July 31, 2015',
+    runtime: '2h 11m',
+    src: '/mi/mi5.jpg',
+    title: 'Mission: Impossible - Rogue Nation'
+  },
+  {
+    rating: 'PG-13',
+    release_date: 'July 27, 2018',
+    runtime: '2h 28m',
+    src: '/mi/mi6.jpg',
+    title: 'Mission: Impossible - Fallout'
+  },
+  {
+    rating: 'PG-13',
+    release_date: 'July 12, 2023',
+    runtime: '2h 43m',
+    src: '/mi/mi7.jpg',
+    title: 'Mission: Impossible - Dead Reckoning Part One'
+  },
+  // {
+  //   rating: 'PG-13',
+  //   release_date: 'June 28, 2024',
+  //   runtime: '2h 4m',
+  //   src: '/mi/mi8.jpg',
+  //   title: 'Mission: Impossible - Dead Reckoning Part Two'
+  // }
+];
+
+const skippedMissionImpossibleMovies = [];
